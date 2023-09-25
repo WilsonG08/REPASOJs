80409b8e72ae6e28f9afad69de7af5b4a046e9f1
Wilson-Guayanay <walex2691@gmail.com>
2023-09-25 18:08:04 -0500

    final cap 3

--- 03-Bases-Node/app.js
+++ 03-Bases-Node/app.js
@@ -1,19 +1,21 @@
+const { crearArchivo } = require('./helpers/multilicar');
+const colors = require('colors');
+const argv = require('./config/yargs');
 
-const { crearArchivo } = require('./helpers/multilicar')
+        
 
 console.clear();
 
-console.log(process.argv)
-
+/* console.log(process.argv)
 const [ , , arg3 = 'base = 5'] = process.argv;
 const [ , base = 5 ] = arg3.split('=');
-console.log(base);
+console.log(base); */
 
 
 //const base = 10;
 
-crearArchivo( base )
-    .then((nombreArchivo) => console.log(nombreArchivo, 'creado'))
-    .catch((err) => console.log(err));
+ crearArchivo( argv.b, argv.l, argv.h )
+    .then((nombreArchivo) => console.log(nombreArchivo.rainbow, 'creado'))
+    .catch((err) => console.log(err)); 
 
 
--- 03-Bases-Node/helpers/multilicar.js
+++ 03-Bases-Node/helpers/multilicar.js
@@ -2,24 +2,34 @@ const { rejects } = require('assert');
 const fs = require('fs');
 const { resolve } = require('path');
 const { monitorEventLoopDelay } = require("perf_hooks");
+const colors = require('colors');
 
 
-const crearArchivo = async (base = 6) => {
+const crearArchivo = async (base = 6, listar = false, hasta=10) => {
 
     try {
-        console.log('==============');
-        console.log('Tabla del:', base);
-        console.log('==============');
 
-        let salida = '';
+        let salida = '';;
+        let consola ='';
 
-        for (i = 1; i <= 10; i++) {
+        for (i = 1; i <= hasta; i++) {
             salida += (`${base} x ${i} = ${base * i}\n`);
+            consola += (`${base} ${ 'x'.green } ${i} ${ '='.green } ${base * i}\n`);
         }
 
-        console.log(salida)
+        if (listar) {
+            
+            console.log('=============='.green);
+            console.log('Tabla del:'.green, colors.blue(base));
+            console.log('=============='.green);
+            
+            console.log(consola)
+        }
 
-        fs.writeFileSync(`tabla-${base}.txt`, salida);
+        
+
+
+        fs.writeFileSync(`./salida/tabla-${base}.txt`, salida);
 
         return `tabla-${base}.txt`;
     } catch (err) {
